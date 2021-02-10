bc1faa29201429ef8f9d445184374acb34643e39
DIEGO FEDER <63731464+diegovfeder@users.noreply.github.com>
2021-02-10 00:47:49 -0300

    Added two new files into src folder, need to dev these new features. Added support for styled components

---
+++ src/Blockchain/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+import { Container } from "./styles";
+
+// TODO:
+const Blockchain: React.FC = () => {
+  return (
+    <Container>
+      <Text>Blockchain</Text>
+    </Container>
+  );
+};
+
+export default Blockchain;
---
+++ src/DogeCoin/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Text} from 'react-native';
+
+import {Container} from './styles';
+
+const DogeCoin: React.FC = () => {
+  return (
+    <Container>
+      <Text>DogeCoin</Text>
+    </Container>
+  );
+};
+
+export default DogeCoin;
